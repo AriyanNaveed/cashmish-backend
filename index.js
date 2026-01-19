8a568b2c0d532ca46875a07f0ee49e0506e42eee
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-01-19 12:35:43 +0500

    restructure database connection and setup DNS configuration

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 import userRoutes from './routes/userRoute.js';
+import { connectDB } from './config/db.js';
 
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -18,14 +17,8 @@ app.get('/', (req, res) => {
 
 app.use("/api/user", userRoutes);
 
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log('MongoDB connected');
+connectDB();
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
