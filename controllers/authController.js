3f8eb9e3ccc2e7b1a63ade696e7080005ed3a1c1
Muttahar Sheikh <153789569+muttahar123@users.noreply.github.com>
2026-02-02 18:57:34 +0500

    feat: add deleteUser function to remove a user by ID

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,4 +48,14 @@ export const getUsers = async (req, res) => {
     res.status(500).json({ message: 'Server error', error: err });
   }
 };
+//delete user
+export const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.json({ message: 'User deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Server error', error: err });
+  }
+};
 
